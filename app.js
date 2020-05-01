141f54144c61fe86eb3a5d98ee5e2a48840e3760
Insidium <ryanchallen@outlook.com>
2020-05-02 09:57:22 +1000

    Added animation for pad size scale up on play loop and added logic for checking if pad is active and then playing appropriate audio

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ class DrumKit {
     //start at the first pad (index 0)
     this.index = 0;
     //set default beats per minute
-    this.bpm = 150;
+    this.bpm = 250;
   }
   //method to toggle "active" class styling to an active pad
   activePad() {
@@ -24,7 +24,24 @@ class DrumKit {
     let step = this.index % 8;
     //select each pad on each row as it steps through
     const activeBars = document.querySelectorAll(`.b${step}`);
-
+    //loop over pads
+    activeBars.forEach((bar) => {
+      //add animation for slight scale up on each pad during loop
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      //check if pads are active
+      if (bar.classList.contains("active")) {
+        //check which sound to play and play it
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.play();
+        }
+      }
+    });
     //increment index by one
     this.index++;
   }
@@ -44,7 +61,12 @@ const drumKit = new DrumKit();
 
 //loop over each pad
 drumKit.pads.forEach((pad) => {
+  //on click, toggle active pad colour via class
   pad.addEventListener("click", drumKit.activePad);
+  //once animation ends, reset so it can start again
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 
 //add event listener for click on play button
